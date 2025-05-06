8429ed0bd45679c8d9e930f3a7ae5adb70640f0b
Martin Diges <mdiges@wisc.edu>
2025-05-05 22:34:11 -0500

    Separated GUI and data analysis into separate documents, added constants.py file for column names and such

---
+++ course_project/constants.py
@@ -0,0 +1,21 @@
+# Run it!
+cn_TIMESTAMP = 'Timestamp'
+cn_TIME_REL_ANCHOR = 'Time Relative to Anchor Event'
+
+cn_PUPIL_DIAM = 'Pupil Diameter'
+cn_TRIAL_IDX = 'Trial Index'
+
+# time columns
+cn_TIME_BEG = 'Time Beg' # time at which trial begins
+cn_TIME_BEHAVIOR = 'Time Behavior' # time at which behavior/action is taken
+cn_TIME_REWARD = 'Time Reward' # time at which reward is shown/given/administered
+cn_TIME_END = 'Time End' # time at which trial ends
+# having all of these timestamps allows us to retrieve pupil samples for each trial individually
+
+cn_SETUP_CHOICE_CORRECT = 'Setup Correct Choice'
+cn_RESULT_CHOICE_USR = 'Choice by User'
+cn_RESULT_CHOICE_PRG = 'Choice by Program'
+cn_RESULT_CORRECTNESS = 'Correct'
+
+columns_events = [cn_TRIAL_IDX, cn_TIME_BEG, cn_TIME_BEHAVIOR, cn_TIME_REWARD, cn_TIME_END,
+                  cn_RESULT_CHOICE_USR, cn_RESULT_CHOICE_PRG, cn_SETUP_CHOICE_CORRECT, cn_RESULT_CORRECTNESS]
